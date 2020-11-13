3ab19c32119b7926e0630672e87d1924f37aee6a
Gendina17 <gendina.nina1@gmail.com>
2020-11-14 02:54:58 +0300

    начало работы с бд

--- app/models.py
+++ app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=256, verbose_name='Имя')
+    birthday = models.DateField(verbose_name='Дата рождения')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Авторы'
+        verbose_name_plural = 'Авторы'
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=1024, verbose_name='Заголовок')
+    text = models.TextField(verbose_name='Текст')
+    # date_create = models.DateField(auto_now_add=True, verbose_name='Дата создания')
+    author = models.ForeignKey('Author', on_delete=models.CASCADE)
+    
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статья'
